68e5f5b9a0287f490b5e3c2da67593afcaa69ad5
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-08-05 22:38:49 +0300

    Added tests for various POST cases

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -22,6 +22,50 @@ describe('/api/blogs', () => {
             expect(response.body[0].id).toBeDefined()
         }
     })
+
+    test('POST adds a blog to the database', async () => {
+        const likes = Math.round(Math.random()*20)
+        const newBlog = {title: 'The History of Testing', url: 'http://www.historyoftesting.example', likes: likes}
+        
+        const response = await api.post('/api/blogs').send(newBlog)
+        expect(response.status).toBe(201)
+
+        const getResponse = await api.get('/api/blogs')
+        expect(getResponse.body).toHaveLength(helper.initialData.length + 1)
+        expect(getResponse.body[getResponse.body.length-1]).toMatchObject(newBlog)
+    })
+
+    test('POST returns the added blog', async () => {
+        const likes = Math.round(Math.random()*20)
+        const newBlog = {title: 'The History of Testing', url: 'http://www.historyoftesting.example', likes: likes}
+        
+        const response = await api.post('/api/blogs').send(newBlog)
+        expect(response.body.title).toBe(newBlog.title)
+        expect(response.body.url).toBe(newBlog.url)
+        expect(response.body.likes).toBe(likes)
+    })
+
+    test('POST likes default to 0', async () => {
+        const newBlog = {title: 'The History of Testing', url: 'http://www.historyoftesting.example'}
+
+        const response = await api.post('/api/blogs').send(newBlog)
+        expect(response.body.likes).toBeDefined()
+        expect(response.body.likes).toBe(0)
+    })
+
+    test('POST with invalid data returns 400', async () => {
+        const empty = {}
+        const noName = {url: 'http://www.historyoftesting.example'}
+        const noUrl = {title: 'The History of Testing'}
+        
+        const emptyResponse = await api.post('/api/blogs').send(empty)
+        const noNameResponse = await api.post('/api/blogs').send(noName)
+        const noUrlResponse = await api.post('/api/blogs').send(noUrl)
+        
+        expect(emptyResponse.status).toBe(400)
+        expect(noNameResponse.status).toBe(400)
+        expect(noUrlResponse.status).toBe(400)
+    })
 })
 
 afterAll(() => {
