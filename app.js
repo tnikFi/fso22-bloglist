564c59c32a169885b7dec08827724f8f2eaf8586
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-08-05 15:43:33 +0300

    Created module for blogs router

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const blogRouter = require('./controllers/blogs')
 
 const MONGODB_URI = process.env.MONGODB_URI
 mongoose.connect(MONGODB_URI)
@@ -10,22 +10,6 @@ mongoose.connect(MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-    
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
+app.use('/api/blogs', blogRouter)
 
 module.exports = app
