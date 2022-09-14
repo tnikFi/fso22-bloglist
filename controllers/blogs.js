14374ce4861a2239ea908759b1b642f92edea154
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-09-14 21:12:16 +0300

    Fixed liking blogs not created by current user

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -32,12 +32,23 @@ blogRouter.put('/:id', async (request, response) => {
     
     // If the blog does not exist, return 404
     if (!blog) return response.status(404).json({error: 'not found'})
-    // If the user id does not match the user id in the blog entry, don't allow editing
-    if (!(blog.user.toString() === request.user.id.toString())) return response.status(401).json({error: 'token missing or invalid'})
 
-    const {title, author, url, likes} = request.body
-    const result = await Blog.findByIdAndUpdate(request.params.id, {title, author, url, likes}, { new: true, runValidators: true, context: 'query' })
-    response.json(result)
+    // Edit the blog data if the user owns it.
+    // If the user doesn't own the blog, check if the amount of likes is 1 more than the current amount.
+    // If this is the case, add 1 to the blog likes.
+    // In all other cases, return 401.
+    if (blog.user.toString() === request.user.id.toString()) {
+        const {title, author, url, likes} = request.body
+        const result = await Blog.findByIdAndUpdate(request.params.id, {title, author, url, likes}, { new: true, runValidators: true, context: 'query' })
+        response.json(result)
+    } else if (blog.likes === request.body.likes - 1) {
+        const newData = {...blog, likes: blog.likes+1}
+        const {title, author, url, likes} = newData
+        const result = await Blog.findByIdAndUpdate(request.params.id, {title, author, url, likes}, { new: true, runValidators: true, context: 'query' })
+        response.json(result)
+    } else {
+        return response.status(401).json({error: 'token missing or invalid'})
+    }
 })
 
 blogRouter.delete('/:id', async (request, response) => {
