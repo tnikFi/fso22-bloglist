26931441e99692332eb16b66121577cf43313837
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-11-06 15:29:35 +0200

    Improved MongoDB environment selection

--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,19 @@
 require('dotenv').config()
 
-const MONGODB_URI = process.env.NODE_ENV === 'test'
-    ? process.env.TEST_MONGODB_URI
-    : process.env.MONGODB_URI
+const getEnvConfig = () => {
+    switch (process.env.NODE_ENV) {
+        case 'test':
+            return process.env.TEST_MONGODB_URI
+        case 'development':
+            return process.env.TEST_MONGODB_URI
+        default:
+            return process.env.MONGODB_URI
+    }
+}
+
+const MONGODB_URI = getEnvConfig()
+
+console.log(MONGODB_URI);
 
 const PORT = process.env.PORT
 const SECRET = process.env.SECRET
